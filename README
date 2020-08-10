ad6ad37f7efb90d36a1853ba4ef013097a110ed5
Fabian <>
2020-08-10 11:40:54 +0200

    added readme

---
+++ README
@@ -0,0 +1,12 @@
+REN
+===
+
+REN is the acronym for ROI Extraction Net and it offers the automatic extraction of ROIs as binary mask matrix in (microscopic) images using a CNN implemented in Tensorflow.
+
+Installation
+------------
+
+First download the zipped repository from here or clone the repository, then install the module with
+
+.. code::
+	$ pip install -e path_to_package/
--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(name = 'REN',
       version = '0.1',
-      description = 'ROI Extraction for microscope images using a CNN',
+      description = 'ROI Extraction Net: supervised ROI extraction for microscope images using a CNN',
       author = 'Fabian Wilde',
       author_email = 'fabian.wilde@uni-greifswald.de',
-      url = 'https://github.com/fwilde,
-      install = ['numpy','scipy','matplotlib', 'tensorflow']
+      url = 'https://github.com/fwilde/REN,
+      install = ['numpy', 'scipy', 'matplotlib', 'tensorflow', \
+                 'read-roi', 'tqdm', 'skimage', 'seaborn']
      )
