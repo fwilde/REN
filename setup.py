e1ec1e01c8d4bb8f6007ee0d4ffd1f158b95a5ed
Fabian <>
2020-08-10 10:32:06 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = 'REN',
+      version = '0.1',
+      description = 'ROI Extraction for microscope images using a CNN',
+      author = 'Fabian Wilde',
+      author_email = 'fabian.wilde@uni-greifswald.de',
+      url = 'https://github.com/fwilde,
+      install = ['numpy','scipy','matplotlib', 'tensorflow']
+     )
